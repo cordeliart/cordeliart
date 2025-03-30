127b0bbbf67e907d553f7742d80ad09688575d87
Lena Pang / Cordeliart <98497328+cordeliart@users.noreply.github.com>
2025-03-30 15:56:21 -0400

    update smtp

--- kiosk/script.js
+++ kiosk/script.js
@@ -5,13 +5,14 @@ const body = document.querySelector("body"),
   shirt = body.querySelector(".shirt"),
   pantsselect = body.querySelector(".pantsselect"),
   pants = body.querySelector(".pants"),
-  sss = body.querySelectorAll('.slimg'),
   camera = body.querySelector('.camera'),
   sender = body.querySelector('.sender'),
   bg = body.querySelector('.bg'),
   sendConfirm = sender.querySelector('#sendConfirm'),
   // cancel = body.querySelector('#cancel'),
   exit = body.querySelector('.exit'),
+  exiter1 = body.querySelector('.exiter1'),
+  exiter2 = body.querySelector('.exiter2'),
   zoom = body.querySelector('.mover'),
   backer = body.querySelector('.backer'),
   credentials = {
@@ -25,7 +26,8 @@ const body = document.querySelector("body"),
   };
 var munson;
 var address;
-var zoomNow = 'full';
+
+// SPLIDE
 
 document.querySelectorAll('.splide').forEach(carousel => new Splide( carousel,{
   type   : 'loop',
@@ -33,22 +35,34 @@ document.querySelectorAll('.splide').forEach(carousel => new Splide( carousel,{
   perMove: 1,
   pagination: false,
   gap: 0,
+  clones: 2,
   // focus: 'center',
 }).mount());
+const spclones = body.querySelectorAll('.slide__slide--clone');
+for (var i = 0; i < spclones.length; ++i) {
+  spclones[i].classList.add('slimg');
+}
+const scaling = body.querySelectorAll('.slimg');
 
 // BASIC ZOOM IN AND OUT
 
 function draw() {}
 
-head.addEventListener("click", () =>{
+headselect.addEventListener("click", () =>{
+  // click  hat
   close();
   head.classList.add("open");
   exit.classList.add('open');
   camera.classList.add('hidden');
+  exiter1.style.display = 'block';
+  exiter2.style.display = 'block';
+  headselect.style.display = 'none';
+  shirtselect.style.display = 'none';
+  pantsselect.style.display = 'none';
 
   bg.style.transform = 'scale(1.8) translate(15vh,25vh)';
-  for (var i = 0; i < sss.length; ++i) {
-    sss[i].classList.add('scaler');
+  for (var i = 0; i < scaling.length; ++i) {
+    scaling[i].classList.add('scaler');
   }
 
   head.style.top = '40vh';
@@ -56,98 +70,138 @@ head.addEventListener("click", () =>{
   pants.style.top = '92.5vh';
 })
 
-shirt.addEventListener("click", () =>{
+shirtselect.addEventListener("click", () =>{
+  // click shirt
   close();
   shirt.classList.add("open");
   exit.classList.add('open');
   camera.classList.add('hidden');
+  exiter1.style.display = 'block';
+  exiter2.style.display = 'block';
+  headselect.style.display = 'none';
+  shirtselect.style.display = 'none';
+  pantsselect.style.display = 'none';
 
   bg.style.transform = 'scale(1.6) translate(15vh,6vh)';
-  for (var i = 0; i < sss.length; ++i) {
-    sss[i].classList.add('scaler');}
+  for (var i = 0; i < scaling.length; ++i) {
+    scaling[i].classList.add('scaler');}
 
   head.style.top = '10vh';
   shirt.style.top = '29.5vh';
   pants.style.top = '62.5vh';
 })
 
-pants.addEventListener("click", () =>{
+pantsselect.addEventListener("click", () =>{
+  // click pants
   close();
   pants.classList.add("open");
   exit.classList.add('open');
   camera.classList.add('hidden');
+  exiter1.style.display = 'block';
+  headselect.style.display = 'none';
+  shirtselect.style.display = 'none';
+  pantsselect.style.display = 'none';
 
   bg.style.transform = 'scale(1.5) translate(15vh,-11vh)';
-  for (var i = 0; i < sss.length; ++i) {
-    sss[i].classList.add('scaler');}
+  for (var i = 0; i < scaling.length; ++i) {
+    scaling[i].classList.add('scaler');}
 
   head.style.top = '-23vh';
   shirt.style.top = '-3.5vh';
   pants.style.top = '29.5vh';
 })
 
-camera.addEventListener("click", () =>{
-  sender.classList.add('sending');
-  zoom.classList.add('moveover');
-  camera.classList.add('hidden');
+exit.addEventListener("click", () =>{
+  // listens for exit
+  zoomOut();
+  close();
 })
 
-exit.addEventListener("click", () =>{
+exiter1.addEventListener("click", () =>{
+  // listens for exit
+  zoomOut();
   close();
+})
+
+exiter2.addEventListener("click", () =>{
+  // listens for exit
+  zoomOut();
+  close();
+})
+
+function zoomOut() {
+  // zooms out
   bg.style.transform = 'scale(1)';
   bg.style.top = '-5vh';
   bg.style.bottom = '-5vh';
   bg.style.left = '-0';
-  for (var i = 0; i < sss.length; ++i) {
-    sss[i].classList.remove('scaler');}
+  for (var i = 0; i < scaling.length; ++i) {
+    scaling[i].classList.remove('scaler');}
 
   head.style.top = '15vh';
   shirt.style.top = '28vh';
   pants.style.top = '50vh';
-})
+  headselect.style.display = 'block';
+  shirtselect.style.display = 'block';
+  pantsselect.style.display = 'block';
+}
 
 function close() {
+  // hide everything (from both zoom and camera move)
   head.classList.remove("open");
   shirt.classList.remove("open");
   pants.classList.remove("open");
   exit.classList.remove('open');
   camera.classList.remove('hidden');
+  exiter1.style.display = 'none';
+  exiter2.style.display = 'none';
 }
 
-// CAMERA STUFF
+// SHARING
 
-// exit camera
+camera.addEventListener("click", () =>{
+  // click share
+  sender.classList.add('sending');
+  zoom.classList.add('moveover');
+  camera.classList.add('hidden');
+})
+
 backer.addEventListener('click', () =>{
+  // exit share
   zoom.classList.remove('moveover');
   sender.classList.remove('sending');
   camera.classList.remove('hidden');
 })
 
-sendConfirm.addEventListener("click", () =>{
-  address = document.getElementById("emailadd").value;
-  console.log(address);
+// EMAILING
 
+sendConfirm.addEventListener("click", () =>{
+  // saves input
+  address = document.getElementById("emailadd").value;
+
+  // sends email
   html2canvas(document.querySelector("#capture")).then(canvas => {
     Email.send({
-      Host: 'smtp.gmail.com',
-      Port: 587,
+      Host: 's1.maildns.net',
+      Port: 465,
       Secure: true,
       Username: 'munsoninstallation@gmail.com',
-      Password: 'vujr xjmj rtky wfya',
+      // Password: 'vujr xjmj rtky wfya',
       To : address,
       From : "you@gmail.com",
-      Subject : "Build-a-Munson",
+      Subject : "Your Build-a-Munson",
       Body : "Look at Munson go!!",
       Attachments : [
-      {
-        name : "munson.png",
-        data : canvas.toDataURL()
-      }]
+        {
+          name : "munson.png",
+          data : canvas.toDataURL()
+        }]
     }).then(
       message => alert(message)
     );
   })
   
+  // resets to screen
   zoom.classList.remove('moveover');
   sender.classList.remove('sending');
   camera.classList.remove('hidden');
